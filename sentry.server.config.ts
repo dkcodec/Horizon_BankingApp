d7af331aa83d12c934c36909cea4711c54415c46
Kairgeldin Dmitry <dimgxj@gmail.com>
2024-06-30 20:10:00 +0500

    fix sentry config

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -12,11 +12,4 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-  integrations: [Sentry.replayIntegration()],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0,
 })
