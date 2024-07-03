20f241415841d3d3a5d9a3a5d668d9f05bfe0569
Kairgeldin Dmitry <dimgxj@gmail.com>
2024-07-03 22:38:17 +0500

    fixing connecting bank

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -6,15 +6,21 @@ import {
   usePlaidLink,
 } from 'react-plaid-link'
 import { useRouter } from 'next/navigation'
-import { createLinkToken } from '@/lib/actions/user.actions'
+import {
+  createLinkToken,
+  exchangePublicToken,
+} from '@/lib/actions/user.actions'
+import Image from 'next/image'
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   const router = useRouter()
+
   const [token, setToken] = useState('')
 
   useEffect(() => {
     const getLinkToken = async () => {
       const data = await createLinkToken(user)
+
       setToken(data?.linkToken)
     }
 
@@ -23,10 +29,11 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
 
   const onSuccess = useCallback<PlaidLinkOnSuccess>(
     async (public_token: string) => {
-      //   await exchangePublicToken({
-      //     publicToken: public_token,
-      //     user,
-      //   })
+      await exchangePublicToken({
+        publicToken: public_token,
+        user,
+      })
+
       router.push('/')
     },
     [user]
@@ -43,16 +50,38 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
     <>
       {variant === 'primary' ? (
         <Button
-          className='plaidlink-primary'
           onClick={() => open()}
           disabled={!ready}
+          className='plaidlink-primary'
         >
           Connect bank
         </Button>
       ) : variant === 'ghost' ? (
-        <Button>Connect bank</Button>
+        <Button
+          onClick={() => open()}
+          variant='ghost'
+          className='plaidlink-ghost'
+        >
+          <Image
+            src='/icons/connect-bank.svg'
+            alt='connect bank'
+            width={24}
+            height={24}
+          />
+          <p className='hiddenl text-[16px] font-semibold text-black-2 xl:block'>
+            Connect bank
+          </p>
+        </Button>
       ) : (
-        <Button>Connect bank</Button>
+        <Button onClick={() => open()} className='plaidlink-default'>
+          <Image
+            src='/icons/connect-bank.svg'
+            alt='connect bank'
+            width={24}
+            height={24}
+          />
+          <p className='text-[16px] font-semibold text-black-2'>Connect bank</p>
+        </Button>
       )}
     </>
   )
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -134,7 +134,7 @@ export const createBankAccount = async ({
   accountId,
   accessToken,
   fundingSourceUrl,
-  sharableId,
+  shareableId,
 }: createBankAccountProps) => {
   try {
     const { database } = await createAdminClient()
@@ -149,7 +149,7 @@ export const createBankAccount = async ({
         accountId,
         accessToken,
         fundingSourceUrl,
-        sharableId,
+        shareableId,
       }
     )
 
@@ -162,6 +162,7 @@ export const exchangePublicToken = async ({
   user,
 }: exchangePublicTokenProps) => {
   try {
+    // Exchange public token for access token and item ID
     const response = await plaidClient.itemPublicTokenExchange({
       public_token: publicToken,
     })
@@ -169,12 +170,14 @@ export const exchangePublicToken = async ({
     const accessToken = response.data.access_token
     const itemId = response.data.item_id
 
-    const accounResponse = await plaidClient.accountsGet({
+    // Get account information from Plaid using the access token
+    const accountsResponse = await plaidClient.accountsGet({
       access_token: accessToken,
     })
 
-    const accountData = accounResponse.data.accounts[0]
+    const accountData = accountsResponse.data.accounts[0]
 
+    // Create a processor token for Dwolla using the access token and account ID
     const request: ProcessorTokenCreateRequest = {
       access_token: accessToken,
       account_id: accountData.account_id,
@@ -186,29 +189,34 @@ export const exchangePublicToken = async ({
     )
     const processorToken = processorTokenResponse.data.processor_token
 
+    // Create a funding source URL for the account using the Dwolla customer ID, processor token, and bank name
     const fundingSourceUrl = await addFundingSource({
       dwollaCustomerId: user.dwollaCustomerId,
       processorToken,
       bankName: accountData.name,
     })
 
+    // If the funding source URL is not created, throw an error
     if (!fundingSourceUrl) throw Error
 
+    // Create a bank account using the user ID, item ID, account ID, access token, funding source URL, and shareableId ID
     await createBankAccount({
       userId: user.$id,
       bankId: itemId,
       accountId: accountData.account_id,
       accessToken,
       fundingSourceUrl,
-      sharableId: encryptId(accountData.account_id),
+      shareableId: encryptId(accountData.account_id),
     })
 
+    // Revalidate the path to reflect the changes
     revalidatePath('/')
 
+    // Return a success message
     return parseStringify({
       publicTokenExchange: 'complete',
     })
   } catch (error) {
-    console.log('Error', error)
+    console.error('An error occurred while creating exchanging token:', error)
   }
 }
